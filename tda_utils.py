3eee1479c5982219ad5f004f75a9ea4873e3d82e
gdepaul <gregory.depaul@gmail.com>
2024-10-28 16:37:53 -0700

    Updating Repo

--- tda_utils.py
+++ tda_utils.py
@@ -6,6 +6,100 @@ import itertools
 import plotly.express as px
 import math 
 from tqdm import tqdm
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def draw_2d_simplicial_complex(simplicial_complex, pos=None, return_pos=False, ax = None):
+    """
+    Draw a simplicial complex up to dimension 2 from a list of simplices, as in [1].
+        
+        Args
+        ----
+        simplicial_complex: a SAGE SimplicialComplex structure. (Added by Greg DePaul)
+        
+        pos: dict (default=None)
+            If passed, this dictionary of positions d:(x,y) is used for placing the 0-simplices.
+            The standard nx spring layour is used otherwise.
+           
+        ax: matplotlib.pyplot.axes (default=None)
+        
+        return_pos: dict (default=False)
+            If True returns the dictionary of positions for the 0-simplices.
+            
+        References
+        ----------    
+        .. [1] I. Iacopini, G. Petri, A. Barrat & V. Latora (2019)
+               "Simplicial Models of Social Contagion".
+               Nature communications, 10(1), 2485.
+               
+        Authors: 
+        .. I. Iacopini (Original Author) - github-username: iaciac
+        .. G. DePaul - github-username: gdepaul
+    """
+
+    # Obtain simplices from SAGE simplicial complex
+    vertices = set()
+    for face in simplicial_complex.facets():
+        for vertex in list(face):
+            vertices.add(vertex)
+    vertex_list = list(vertices)
+    node_names_to_index = { vertex_list[i]:i for i in range(len(vertex_list))}
+
+    simplices = []
+    for face in simplicial_complex.facets():
+        new_face = []
+        for vertex in list(face):
+            new_face.append(node_names_to_index[vertex])
+        simplices.append(new_face)
+    
+    #List of 0-simplices
+    nodes =list(set(itertools.chain(*simplices)))
+    
+    #List of 1-simplices
+    edges = list(set(itertools.chain(*[[tuple(sorted((i, j))) for i, j in itertools.combinations(simplex, 2)] for simplex in simplices])))
+
+    #List of 2-simplices
+    triangles = list(set(itertools.chain(*[[tuple(sorted((i, j, k))) for i, j, k in itertools.combinations(simplex, 3)] for simplex in simplices])))
+    
+    if ax is None: ax = plt.gca()
+    ax.set_xlim([-1.1, 1.1])      
+    ax.set_ylim([-1.1, 1.1])
+    ax.get_xaxis().set_ticks([])  
+    ax.get_yaxis().set_ticks([])
+    ax.axis('off')
+       
+    if pos is None:
+        # Creating a networkx Graph from the edgelist
+        G = nx.Graph()
+        G.add_edges_from(edges)
+        # Creating a dictionary for the position of the nodes
+        pos = nx.spring_layout(G)
+        
+    # Drawing the edges
+    for i, j in edges:
+        (x0, y0) = pos[i]
+        (x1, y1) = pos[j]
+        line = plt.Line2D([ x0, x1 ], [y0, y1 ],color = 'black', zorder = 1, lw=0.7)
+        ax.add_line(line);
+    
+    # Filling in the triangles
+    for i, j, k in triangles:
+        (x0, y0) = pos[i]
+        (x1, y1) = pos[j]
+        (x2, y2) = pos[k]
+        tri = plt.Polygon([ [ x0, y0 ], [ x1, y1 ], [ x2, y2 ] ],
+                          edgecolor = 'black', facecolor = plt.cm.Blues(0.6),
+                          zorder = 2, alpha=0.4, lw=0.5)
+        ax.add_patch(tri);
+
+    # Drawing the nodes 
+    for i in nodes:
+        (x, y) = pos[i]
+        circ = plt.Circle([ x, y ], radius = 0.02, zorder = 3, lw=0.5,
+                          edgecolor = 'Black', facecolor = u'#ff7f0e')
+        ax.add_patch(circ);
+
+    if return_pos: return pos
 
 class SimplexTree: 
     
@@ -121,21 +215,21 @@ def draw_geometric_simplcial_complex(cplx, S, draw_points = True, draw_edges = T
             )
     
     if draw_surfaces:
-        i = []
-        j = []
-        k = []
-        for simplex, val in cplx[3]:
-            if len(simplex) == D + 1:
-                for idx_1, idx_2, idx_3 in list(itertools.combinations(simplex, 3)):
-                    i.append(node_2_idx[idx_1])
-                    j.append(node_2_idx[idx_2])
-                    k.append(node_2_idx[idx_3])
+#         i = []
+#         j = []
+#         k = []
+#         for simplex, val in cplx[3]:
+#             if len(simplex) == D + 1:
+#                 for idx_1, idx_2, idx_3 in list(itertools.combinations(simplex, 3)):
+#                     i.append(node_2_idx[idx_1])
+#                     j.append(node_2_idx[idx_2])
+#                     k.append(node_2_idx[idx_3])
 
-        i = np.array(i)
-        j = np.array(j)
-        k = np.array(k)
+#         i = np.array(i)
+#         j = np.array(j)
+#         k = np.array(k)
 
-        things_to_plot.append(go.Mesh3d(x=S[:,0], y=S[:,1], z=S[:,2],alphahull=5, opacity=0.4, color='purple', i=i, j=j, k=k,hoverinfo='none'))
+#         things_to_plot.append(go.Mesh3d(x=S[:,0], y=S[:,1], z=S[:,2],alphahull=5, opacity=0.4, color='purple', i=i, j=j, k=k,hoverinfo='none'))
 
         i = []
         j = []
